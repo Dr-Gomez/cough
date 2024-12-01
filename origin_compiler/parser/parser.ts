a503c2aca9e1c161fa66e97a78b36b4826b0c93b
Marco A. Gomez <gomez.contact.mgz@gmail.com>
2024-12-01 17:10:13 +0000

    replaces AST node interfaces with AST classes

--- origin_compiler/parser/parser.ts
+++ origin_compiler/parser/parser.ts
@@ -4,13 +4,63 @@ abstract class Node {
   abstract type: string;
 }
 
-class NumberNode extends Node {
-  type = "number";
+// Control Flow Nodes
+
+export class CodeBlockNode extends Node {
+  type = "CodeBlock";
+  statements: Node[];
+
+  constructor(statements: Node[]) {
+    super();
+    this.statements = statements;
+  }
+}
+
+// Literal Nodes
+
+class NumberLiteralNode extends Node {
+  type = "Number";
   value: number;
 
   constructor(value: number) {
     super();
     this.value = value;
+  }
+}
+
+class StringLiteralNode extends Node {
+  type = "String";
+  value: string;
+
+  constructor(value: string) {
+    super();
+    this.value = value;
+  }
+}
+
+class BoolLiteralNode extends Node {
+  type = "Boolean";
+  value: boolean;
+
+  constructor(value: boolean) {
+    super();
+    this.value = value;
+  }
+}
+
+// Operation Nodes
+
+class BinaryOperationNode extends Node {
+  type = "BinaryOperation";
+  left: Node;
+  operator: string;
+  right: Node;
+
+  constructor(left: Node, operator: string, right: Node) {
+    super();
+    this.left = left;
+    this.operator = operator;
+    this.right = right;
   }
 }
 
