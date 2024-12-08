86b8fb166bfa21f69708376e33e8099d787d38ac
Marco A. Gomez <gomez.contact.mgz@gmail.com>
2024-12-08 14:12:40 +0000

    changes dummy code to simplify parser development

--- origin_compiler/compiler.ts
+++ origin_compiler/compiler.ts
@@ -5,20 +5,7 @@ import log from "./logs/log.ts";
 
 const sourceCode = `
 
-bool running = true;
-char@array alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-int@array numbers = [16, 1, 19, 20];
-
-int i = 0;
-loop {
-  i++;
-
-  print(alphabet[numbers[i]]);
-
-  if(i == length@numbers){
-    break;
-  }
-}
+12
 
 `;
 
